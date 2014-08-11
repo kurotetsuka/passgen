13a38fcbbcc2dd5beff4092dd7a3d96e5f5af47c
kurotetsuka <kurotetsuka@gmail.com>
2014-08-11 11:10:38 -0400

    init

---
+++ passgen.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/ruby
+
+class Passgen
+	attr_accessor :words
+
+	def initialize( filename = "wordlist.txt")
+		load( filename)
+	end
+
+	def load( filename = "wordlist.txt")
+		data = File.read( filename)
+		@words = data.split("\n")
+	end
+
+	def gen( wc = 5)
+		#setup
+		result = ""
+		old = Array.new
+		rand = Random.new()
+		spaces_enabled = true
+		#while have more words to gen
+		while wc > 0
+			#chose an entry
+			i = rand.rand( @words.length)
+			#disqualify duplicates
+			if old.include?( i)
+				next
+			end
+			#push space
+			if spaces_enabled && ! old.empty?
+				result << " "
+			end
+			#push word
+			result << @words[ i]
+			old.push( i)
+			wc -= 1
+		end
+		return result
+	end
+
+end
+
+if __FILE__ == $0
+	printf( "argv: %s\n", ARGV);
+	#get word count
+	wc = 5
+	if ! ARGV.empty?
+		temp = ARGV[0].to_i
+		if temp != 0
+			wc = temp
+		end
+	end
+	#setup
+	passgen = Passgen.new()
+	password = passgen.gen( wc)
+	#print password
+	read_enabled = true
+	if read_enabled
+		printf( "password: %s\n", password)
+	end
+end
---
+++ readme.md
@@ -0,0 +1,7 @@
+### xkcd-style passgen in ruby
+
+By: [kurotetsuka](https://github.com/kurotetsuka)  
+This work is released under the MIT license. See [license.md](license.md) for details.
+
+Based on: [xkcd #936](https://xkcd.com/936/)  
+[![xkcd-password-comic](https://sslimgs.xkcd.com/comics/password_strength.png)](https://xkcd.com/936/)
